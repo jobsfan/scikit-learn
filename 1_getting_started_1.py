13368adab53478bdb793eb5976f25a64bb7ad997
jobsfan <289047960@qq.com>
2021-10-20 17:30:34 +0800

    getting_started 1

---
+++ 1_getting_started_1.py
@@ -0,0 +1,15 @@
+# from https://scikit-learn.org/stable/getting_started.html
+
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(random_state=0)
+
+X = [[1, 2, 3],
+     [11, 12, 13]]
+
+y = [0, 1]
+clf.fit(X, y)
+aa = clf.predict(X)
+print(aa)
+xx = clf.predict([[4, 5, 6], [14, 15, 16]])
+print(xx)
