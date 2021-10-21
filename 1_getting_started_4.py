4a575b41d5ede02af6f176ed2d7d83d25b97a69d
jobsfan <289047960@qq.com>
2021-10-21 16:43:10 +0800

    linear_model线性模型

--- 1_getting_started_4.py
+++ 1_getting_started_4.py
@@ -5,6 +5,7 @@ from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_validate
 
+# make_regression 生成一个回归问题的数据
 X, y = make_regression(n_samples=1000, random_state=0)
 lr = LinearRegression()
 
---
+++ 2_linear_model_2.py
@@ -0,0 +1,7 @@
+# https://scikit-learn.org/stable/modules/linear_model.html#ridge-regression-and-classification
+
+from sklearn import linear_model
+reg = linear_model.Ridge(alpha=.5)
+reg.fit([[0, 0], [0, 0], [1, 1]], [0, .1, 1])
+print(reg.coef_)
+print(reg.intercept_)
